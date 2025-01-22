9d5ceaf44b823137a09165402d530cc8d5869f4d
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-01-22 14:01:12 -0300

    feat: create movies_orders views and routes for posting a movie order

---
+++ movies_orders/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import MovieOrder
+
+
+class MovieOrderSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(read_only=True, source="movie.title")
+    price = serializers.DecimalField(max_digits=8, decimal_places=2)
+    purchased_by = serializers.CharField(read_only=True, source="user.email")
+    purchased_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data: dict):
+        return MovieOrder.objects.create(**validated_data)
--- movies_orders/views.py
+++ movies_orders/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .serializers import MovieOrderSerializer
+from movies.models import Movie
 
-# Create your views here.
+
+class MovieOrderView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request: Request, movie_id: int) -> Response:
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieOrderSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(movie=movie, user=request.user)
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
